7abbc3ceeb7eac44fff5b48bf241d4356cd23d74
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-27 23:18:27 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,17 +44,17 @@ def naming_system
 kingdom = {
   label: "kingdom",
   sub_category: {
-    label: "Phylum",
-    sub_category: {
-      label: "Class",
-      sub_category:{
-        label: "Order",
-        sub_category: {
-          label: "Family",
-          sub_category: {
-            label: "Genus",
-            sub_category{
-              label: "Species"
+    :label => "Phylum",
+    :sub_category => {
+     :label => "Class",
+      :sub_category => {
+        :label => "Order",
+        :sub_category => {
+          :label => "Family",
+          :sub_category => {
+            :label => "Genus",
+            :sub_category =>{
+              :label => "Species"
             }
           }
         }
